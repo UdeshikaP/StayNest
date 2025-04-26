6b42197fe89e6d06077100281503034c469f017c
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-04-26 16:48:46 +0530

    create backend

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: 'StayNest_db',
+
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+
+// Start the server
+const port = process.env.PORT || 5003;
+app.listen(port, () => console.log(`Node server started at port ${port}`));
+ 
+
+
+
+// Import Routes
+const userRoutes = require("./routes/userRoutes");
+
+
+app.use("/user",userRoutes);
+
+
+
+
+
+
+
+
+
