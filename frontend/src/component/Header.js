aec33bd43ccafe513fb6b5487b264266b50dcceb
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-04-26 16:50:01 +0530

    update header

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -4,18 +4,22 @@ function Header() {
         <nav className="header">
             {/* <div className="logo">Red<span>Planet</span></div> */}
             <ul className="header-links">
-            <li><a href="/#booking">Home</a></li>
-            
-            <li><Link to="/about-us">About</Link></li>
+                <li><a href="/#booking">Home</a></li>
+
+                <li><Link to="/about-us">About</Link></li>
                 <li><Link to="/room-details">Rooms</Link></li>
                 <li><Link to="/menu-item">Menus</Link></li>
                 <li><Link to="/events">Events</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
             <div className="buttons">
-        <button className="login">Login</button>
-        <button className="signup">Sign Up</button>
-      </div>
+                <Link to="/login">
+                    <button className="login">Login</button>
+                </Link>
+                <Link to="/signup">
+                <button className="signup">Sign Up</button>
+                </Link>
+            </div>
         </nav>
 
     );
