5298f2fe8688d493287d7d51f43f773b6883ae61
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-04-26 23:58:44 +0530

    update footer

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Facebook, Instagram, Youtube, Phone } from 'lucide-react';
 import "./Footer.css";
