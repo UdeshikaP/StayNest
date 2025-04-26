2320ff81a467db755c3902b49e1ff61c2d2f4ab9
Udeshika Balasooriya <piumiudeshika02@gmail.com>
2025-04-27 01:39:43 +0530

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,7 @@ import AboutUs from './pages/About/aboutUs';
 import ContactUs from './pages/Contact/contact';
 import Rooms from './pages/Rooms/rooms';
 import Menus from './pages/Menu/menuItem';
-import Events from './pages/Events/events';
-import Footer from './component/Footer';
+ import Footer from './component/Footer';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/signup';
 import './App.css';
@@ -25,8 +24,7 @@ function App() {
           <Route path='/contact' element={<ContactUs/>}/>
           <Route path='/room-details' element={<Rooms/>}/>
           <Route path='/menu-item' element={<Menus/>}/>
-          <Route path='/events' element={<Events/>}/>
-          <Route path='/login' element={<Login/>}/>
+           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
         </Routes>
     </div>
